246a6b53315d7554811c26665d6cbbfda4b97750
dallaechoi <choi032799@gmail.com>
2025-11-14 01:25:01 +0900

    feat : 로그인/회원가입 + 내정보확인

---
+++ src/main/java/com/likelion/backend/config/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.likelion.backend.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        // 설정 추가
+        return http
+                .cors((SecurityConfig::corsAllow)) // Cors 설정
+                .csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화
+                .sessionManagement((manager) -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))// 세션 로그인 안함
+                .httpBasic(AbstractHttpConfigurer::disable) // http basic auth 기반 로그인 인증창 뜨지 않게
+                .formLogin(AbstractHttpConfigurer::disable) // 기본 로그인 페이지 뜨지 않게.
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/login","/my/{id}").permitAll() // 모두 허용
+                        .anyRequest().authenticated())
+                .build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    private static void corsAllow(CorsConfigurer<HttpSecurity> corsConfigurer){
+        corsConfigurer.configurationSource(request -> {
+            CorsConfiguration configuration = new CorsConfiguration();
+
+            configuration.setAllowedMethods(Collections.singletonList("*")); // 모든 메서드 허용
+            configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // 프론트에서 오는 요청 허용
+            configuration.setAllowedHeaders(Collections.singletonList("*")); // 모든 헤더 허용
+            configuration.setAllowCredentials(true);
+            configuration.setMaxAge(3600L); // 1시간(3600초) 동안 오는 요청이 처리됨
+
+            return configuration;
+        });
+    }
+}
---
+++ src/main/java/com/likelion/backend/dto/response/JoinResponseDto.java
@@ -0,0 +1,27 @@
+package com.likelion.backend.dto.response;
+import com.likelion.backend.domain.Member;
+import com.likelion.backend.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class JoinResponseDto {
+    private Long id;
+    private String name;
+    private Role role;
+
+    // DTO 변환
+    public static JoinResponseDto fromEntity(Member member){
+
+        // 질문 결과 list 관련
+
+        return JoinResponseDto.builder()
+                .id(member.getId())
+                .name(member.getName())
+                .role(member.getRole())
+                .build();
+    }
+}
